8a0b27a3b71b69896ad5e9790b039cbd7c5e162c
AudreyLlaca <audrey.llaca@hotmail.com>
2024-12-16 20:49:26 -0600

    final comments

--- applicacion.py
+++ applicacion.py
@@ -6,17 +6,17 @@ import plotly.express as px
 car_data = pd.read_csv('notebooks/vehicles_us.csv')
 
 # Crear un encabezado
-st.header("Aplicación de Streamlit para visualización de datos de coches")
+st.header("Check out the car data)
 
 # Crear un botón
-hist_button = st.button("Construir histograma")
+hist_button = st.button("Build Histogram")
 
 # Crear el histograma cuando se haga clic en el botón
 if hist_button: 
-    st.write("Creación de un histograma para el conjunto de datos de anuncios de venta de coches")
+    st.write("check out the dataset for the car sells")
     
     # Generar el histograma usando Plotly Express
-    fig = px.histogram(car_data, x="odometer", title="Histograma del odómetro")
+    fig = px.histogram(car_data, x="odometer", title="Histogram")
     
     # Mostrar el gráfico Plotly en la app de Streamlit
     st.plotly_chart(fig, use_container_width=True)
