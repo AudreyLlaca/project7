6ef2e9c515fe402b09687109d1f729c59e33610b
AudreyLlaca <audrey.llaca@hotmail.com>
2024-12-16 21:01:33 -0600

    add an exrta button

--- applicacion.py
+++ applicacion.py
@@ -2,21 +2,38 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Leer los datos en un DataFrame
+
 car_data = pd.read_csv('notebooks/vehicles_us.csv')
 
-# Crear un encabezado
+
 st.header("Check out the car data")
 
-# Crear un botón
+
 hist_button = st.button("Build Histogram")
 
-# Crear el histograma cuando se haga clic en el botón
+#Boton1
 if hist_button: 
     st.write("check out the dataset for the car sells")
     
-    # Generar el histograma usando Plotly Express
+   
     fig = px.histogram(car_data, x="odometer", title="Histogram")
     
-    # Mostrar el gráfico Plotly en la app de Streamlit
+  
     st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button("Build scatter plot")
+#boton2
+if scatter_button:
+    st.write("scatter plot between the price and odometer")
+    
+    
+    fig_scatter = px.scatter(
+        car_data, 
+        x="odometer", 
+        y="price", 
+        title="Price vs Odometer",
+        labels={"odometer", "price USD"}
+    )
+    
+   
+    st.plotly_chart(fig_scatter, use_container_width=True)
